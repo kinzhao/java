d4bcd5fc147dcd5581e44c9e3d93e41fa0d6a483
kinzhao <kinsz89@gmail.com>
2020-02-04 11:45:08 -0500

    Working with classes - part 2

--- MySecondApp/src/Driver.java
+++ MySecondApp/src/Driver.java
@@ -2,7 +2,7 @@ public class Driver {
     public static void main(String[] args) {
         Employee liam = new Employee();
         Employee linda = new Employee();
-
+        System.out.println(liam.getName());
         liam.setName("Liam Jones");
         linda.setName("Liam Jones");
         liam.setLanguage("Java");
@@ -12,5 +12,8 @@ public class Driver {
 
         System.out.println(liam.getName() + " makes $" + liam.getSalary() + " and likes " + liam.getLanguage() + " programming language.");
         System.out.println(linda.getName() + " makes $" + linda.getSalary() + " and likes " + linda.getLanguage() + " programming language.");
+
+        Employee david = new Employee("David Jones", 170000,"Java");
+        System.out.println(david.getName() + " makes $" + david.getSalary() + " and likes " + david.getLanguage() + " programming language.");
     }
 }
--- MySecondApp/src/Employee.java
+++ MySecondApp/src/Employee.java
@@ -10,18 +10,42 @@ public class Employee {
     public int getSalary() {return this.salary; }
 
     //Setters
-    public void setName(String newName) {
-        String result[] = newName.split(" ");
-        this.firstName = result[0];
-        this.lastName = result[1];
+    public void setName(String name) {
+        if(name != null) {
+            String result[] = name.split(" ");
+            this.firstName = result[0];
+            this.lastName = result[1];
+        }
     }
 
-    public void setLanguage(String newLanguage) {
-        this.language = newLanguage;
+    public void setLanguage(String language) {
+        if(language != null) {
+            this.language = language;
+        }
     }
 
-    public void setSalary(int newSalary) {
-        this.salary = newSalary;
+    public void setSalary(int salary) {
+        if(salary > 0) {
+            this.salary = salary;
+        }
     }
 
+    public Employee() {
+        this("John Doe");
+    }
+
+    public Employee(String name) {
+        this.setName(name);
+    }
+
+    public Employee(String name, int salary) {
+        this.setName(name);
+        this.setSalary(salary);
+    }
+
+    public Employee(String name, int salary, String language) {
+        this.setName(name);
+        this.setSalary(salary);
+        this.setLanguage(language);
+    }
 }
